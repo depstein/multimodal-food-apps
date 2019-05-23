5d885b385b1a66bba6255e7a59c2d07b3ab9e461
Daniel Epstein <epstein@ics.uci.edu>
2019-05-23 14:10:39 -0700

    Rename home page to login page

---
+++ mobile/src/app/pages/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+
+
+export class LoginPage implements OnInit {
+
+  constructor(private route: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.route.navigateByUrl('home');
+  }
+}
