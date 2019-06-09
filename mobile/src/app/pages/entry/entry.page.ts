a73ceb162b60dac01f9347306f37552a8bc11442
Yuqi Huai <yhuai@uci.edu>
2019-06-09 11:54:50 -0700

    updated page UI

--- mobile/src/app/pages/entry/entry.page.ts
+++ mobile/src/app/pages/entry/entry.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LogService } from 'src/app/services/log.service';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-entry',
@@ -10,46 +11,105 @@ import { LogService } from 'src/app/services/log.service';
 export class EntryPage implements OnInit {
 
   modalities_list = []
-  
+
   constructor(private router: Router,
-              private logService: LogService) { }
+    private logService: LogService,
+    private alertController: AlertController,
+    private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  async onBack() {
+    if (this.logService.data.entries.length !== 0) {
+      // has data
+      const alert = await this.alertController.create({
+        header: 'Are you sure?',
+        message: 'Do you wish to clear all entries before going back to home page?',
+        buttons: [
+          {
+            text: 'Save draft',
+            handler: () => {
+              this.router.navigateByUrl('/home');
+            }
+          }, {
+            text: 'Delete draft',
+            cssClass: 'delete',
+            handler: () => {
+              this.logService.data.entries = [];
+              this.router.navigateByUrl('/home');
+            }
+          }, {
+            text: 'Cancel',
+            role: 'cancel',
+            cssClass: 'secondary',
+            handler: (blah) => {
+              // console.log('Confirm Cancel: blah');
+            }
+          }
+        ]
+      });
+      await alert.present();
+    } else {
+      this.router.navigateByUrl('/home');
+    }
   }
 
   navigate(num: number) {
     switch (num) {
       case 0:
-      this.router.navigateByUrl('e-barcode');
-      break;
+        this.router.navigateByUrl('e-barcode');
+        break;
       case 1:
-      this.router.navigateByUrl('e-description');
-      break;
+        this.router.navigateByUrl('e-description');
+        break;
       case 2:
-      this.router.navigateByUrl('e-search');
-      break;
+        this.router.navigateByUrl('e-search');
+        break;
       case 3:
-      this.router.navigateByUrl('e-voice');
-      break;
+        this.router.navigateByUrl('e-voice');
+        break;
       case 4:
-      this.router.navigateByUrl('e-url');
-      break;
+        this.router.navigateByUrl('e-url');
+        break;
       case 5:
-      this.router.navigateByUrl('e-camera');
-      break;
+        this.router.navigateByUrl('e-camera');
+        break;
     }
   }
 
   onSubmit() {
-    this.logService.setName('Yuqi3');
+    // this.logService.setName('Yuqi3');
     this.logService.setPlatform('ios');
     this.logService.push();
   }
 
-  onRemove(index) {
-    this.logService.data.entries = this.logService.data.entries.filter( (entry) => {
-      return entry !== this.logService.data.entries[index];
+  async onRemove(index) {
+
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure? Deletion is unrecoverable',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Delete',
+          cssClass: 'danger',
+          handler: () => {
+            // console.log('Confirm Okay');
+            this.logService.data.entries = this.logService.data.entries.filter((entry) => {
+              return entry !== this.logService.data.entries[index];
+            });
+          }
+        }
+      ]
     });
+    await alert.present();
   }
 
   onEdit(index) {
