239c0f43e14726fe085fccfe2fe4e06c920f9274
Yuqi Huai <yhuai@uci.edu>
2019-11-05 20:24:45 -0800

    voice interface updated
    voice interace now can delete last log.

--- voice_interface/src/config.js
+++ voice_interface/src/config.js
@@ -4,15 +4,17 @@
 
 module.exports = {
     logging: true,
- 
+
     intentMap: {
-       'AMAZON.StopIntent': 'END',
+        "AMAZON.StopIntent": "END",
+        "AMAZON.StopIntent": "END",
+        "AMAZON.YesIntent": "YesIntent",
+        "AMAZON.NoIntent": "NoIntent"
     },
- 
+
     db: {
-         FileDb: {
-             pathToFile: '../db/db.json',
-         }
-     },
- };
- 
+        FileDb: {
+            pathToFile: "../db/db.json"
+        }
+    }
+};
