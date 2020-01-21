e425b1765e470499cafd669469ef725d489f2c18
Yuqi Huai <yhuai@uci.edu>
2020-01-21 14:40:36 +0800

    fixed date error
    previously using wrong date pipe format

--- mobile v2/src/app/pages/historydate/historydate.page.ts
+++ mobile v2/src/app/pages/historydate/historydate.page.ts
@@ -10,35 +10,33 @@ import { ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-historydate',
   templateUrl: './historydate.page.html',
-  styleUrls: ['./historydate.page.scss'],
+  styleUrls: ['./historydate.page.scss']
 })
 export class HistoryDatePage implements OnInit {
-
   calendar_date: Date;
   private logsCollection: AngularFirestoreCollection<any>;
   logs: Observable<any[]>;
   logs_array: any[];
   pass_date: Date;
 
-
-  constructor(private router: Router,
+  constructor(
+    private router: Router,
     private logService: LogService,
     private afs: AngularFirestore,
-    public modalController: ModalController) {
-
-    this.calendar_date = new Date()
-    this.pass_date = new Date()
-    this.logs_array= new Array()
+    public modalController: ModalController
+  ) {
+    this.calendar_date = new Date();
+    this.pass_date = new Date();
+    this.logs_array = new Array();
 
     this.logsCollection = this.afs.collection(this.logService.username, ref => ref.orderBy('date', 'asc'));
 
     this.logs_array = this.getentries();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
-  dismiss(){
+  dismiss() {
     //closes the modal
     this.modalController.dismiss(this.pass_date);
   }
@@ -47,29 +45,22 @@ export class HistoryDatePage implements OnInit {
     //collects all entries in the database
     const entry = [];
     const logs = this.logsCollection.snapshotChanges();
-    logs.subscribe(
-      array => {
-        array.forEach(
-          element => {
-            const ele = element.payload.doc.data();
-            // checks the date
-            const dates = new EntryCard(ele.date, ele.entries, ele.platform, element.payload.doc.id);
-            entry.push(dates);
-          }
-        );
-      }
-    );
-    return entry
+    logs.subscribe(array => {
+      array.forEach(element => {
+        const ele = element.payload.doc.data();
+        // checks the date
+        console.log(ele.date);
+        const dates = new EntryCard(ele.date, ele.entries, ele.platform, element.payload.doc.id);
+        entry.push(dates);
+      });
+    });
+    return entry;
   }
 
-  getdate(log){
+  getdate(log) {
     //gets the date of the clicked entry
-    var x = new Date(log.date)
+    var x = new Date(log.date);
     this.pass_date = x;
-    this.dismiss()
+    this.dismiss();
   }
-
-
-
-
 }
