6ab56f3323866303510fe43b4ef29e3cbbd94e8a
Yuqi Huai <yhuai@uci.edu>
2020-04-01 15:47:45 -0700

    bug fix
    SpeechRecognitionService is injected only if user is using Chrome. The library is only supported in Chrome.

--- web/src/app/components/input-modal/input-modal.component.ts
+++ web/src/app/components/input-modal/input-modal.component.ts
@@ -1,25 +1,23 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Injector } from "@angular/core";
 
-import { CommunicationService } from 'src/app/services/communication.service';
+import { CommunicationService } from "src/app/services/communication.service";
 
 import {
   RxSpeechRecognitionService,
-  resultList,
-} from '@kamiazya/ngx-speech-recognition';
+  resultList
+} from "@kamiazya/ngx-speech-recognition";
 
-import {
-  SpeechRecognitionService
-} from '@kamiazya/ngx-speech-recognition';
+import { SpeechRecognitionService } from "@kamiazya/ngx-speech-recognition";
 
 declare var $: any;
 
 @Component({
-  selector: 'app-input-modal',
-  templateUrl: './input-modal.component.html',
-  styleUrls: ['./input-modal.component.scss']
+  selector: "app-input-modal",
+  templateUrl: "./input-modal.component.html",
+  styleUrls: ["./input-modal.component.scss"]
 })
 export class InputModalComponent implements OnInit {
-  @ViewChild('file') file;
+  @ViewChild("file") file;
 
   textEntry = "";
   img: any;
@@ -32,22 +30,25 @@ export class InputModalComponent implements OnInit {
 
   showSearchResults = false;
 
+  speechService = null;
+  isChrome = false;
+
   // transcript = '';
   isRecording = false;
-  constructor(private conm: CommunicationService, private speechService: SpeechRecognitionService) {
-    this.speechService.onstart = (e) => {
-    };
-    this.speechService.onresult = (e) => {
-      this.textEntry = e.results[0].item(0).transcript;
-      this.speechService.stop();
-      this.isRecording = false;
-    };
+  constructor(private conm: CommunicationService, private injector: Injector) {
+    this.isChrome = window.navigator.userAgent.indexOf("Chrome") !== -1;
+    if (this.isChrome) {
+      this.speechService = injector.get(SpeechRecognitionService);
+      this.speechService.onstart = () => {};
+      this.speechService.onresult = e => {
+        this.textEntry = e.results[0].item(0).transcript;
+        this.speechService.stop();
+        this.isRecording = false;
+      };
+    }
   }
 
-
-  ngOnInit() {
-
-  }
+  ngOnInit() {}
 
   onListen() {
     // this.speechService.listen().pipe(resultList).subscribe(
@@ -56,30 +57,37 @@ export class InputModalComponent implements OnInit {
     //     console.log('RxComponent:onresult', this.transcript, list);
     //   }
     // );
-    if (!this.isRecording) {
-      this.isRecording = true;
-      this.speechService.start();
+    if (this.isChrome) {
+      if (!this.isRecording) {
+        this.isRecording = true;
+        this.speechService.start();
+      }
+    } else {
+      console.log("Speech Recognition is Only Supported in Chrome");
     }
   }
 
   getName(mode) {
     switch (mode) {
       case 0:
-        this.placeholder = 'Please describe what you ate in whatever form would be most useful for you.';
-        return 'Description';
+        this.placeholder =
+          "Please describe what you ate in whatever form would be most useful for you.";
+        return "Description";
       case 1:
-        this.placeholder = 'Please describe the terms you would use to search from a database of foods commonly eaten and logged.';
-        return 'Search';
+        this.placeholder =
+          "Please describe the terms you would use to search from a database of foods commonly eaten and logged.";
+        return "Search";
       case 2:
-        return 'Image';
+        return "Image";
       case 3:
-        this.placeholder = 'Please enter the URL of a recipe or other website which describes what you ate.';
-        return 'URL';
+        this.placeholder =
+          "Please enter the URL of a recipe or other website which describes what you ate.";
+        return "URL";
       case 4:
-        return 'Voice';
+        return "Voice";
       case 5:
-        this.placeholder = 'Please enter the digits under the barcode';
-        return 'Barcode';
+        this.placeholder = "Please enter the digits under the barcode";
+        return "Barcode";
     }
   }
 
@@ -92,32 +100,34 @@ export class InputModalComponent implements OnInit {
   }
 
   isSearch(mode) {
-    return mode === 1
+    return mode === 1;
   }
-
 
   onClose() {
     this.cleanUp();
-    $('#exampleModal').modal('toggle');
+    $("#exampleModal").modal("toggle");
   }
 
   onSave() {
-    if (this.isText(this.mode) || this.isVoice(this.mode) || this.isSearch(this.mode)) {
+    if (
+      this.isText(this.mode) ||
+      this.isVoice(this.mode) ||
+      this.isSearch(this.mode)
+    ) {
       if (this.editIndex === -1 && this.textEntry.length === 0) {
         return;
       }
       if (this.editIndex === -1) {
         const title = this.getName(this.mode);
 
-        const obj = { 'title': title, 'content': this.textEntry };
+        const obj = { title: title, content: this.textEntry };
         this.conm.draftEntries.push(obj);
-        $('#exampleModal').modal('toggle');
+        $("#exampleModal").modal("toggle");
         this.cleanUp();
       } else {
-
-        this.conm.draftEntries[this.editIndex]['content'] = this.textEntry;
+        this.conm.draftEntries[this.editIndex]["content"] = this.textEntry;
         this.editIndex = -1;
-        $('#exampleModal').modal('toggle');
+        $("#exampleModal").modal("toggle");
         this.cleanUp();
       }
     } else {
@@ -127,27 +137,27 @@ export class InputModalComponent implements OnInit {
       //this is an image
       if (this.editIndex === -1) {
         const title = this.getName(this.mode);
-        const obj = { 'title': title, 'content': this.img };
+        const obj = { title: title, content: this.img };
         this.conm.draftEntries.push(obj);
-        $('#exampleModal').modal('toggle');
+        $("#exampleModal").modal("toggle");
         this.cleanUp();
       } else {
-        this.conm.draftEntries[this.editIndex]['content'] = this.img;
-        $('#exampleModal').modal('toggle');
+        this.conm.draftEntries[this.editIndex]["content"] = this.img;
+        $("#exampleModal").modal("toggle");
         this.cleanUp();
       }
     }
   }
 
   toggleSearchResults() {
-    if (this.textEntry !== ''){
+    if (this.textEntry !== "") {
       return true;
     } else return false;
   }
   onFileChange(file) {
     const reader = new FileReader();
     reader.readAsDataURL(file[0]);
-    reader.onload = (_event) => {
+    reader.onload = _event => {
       this.img = reader.result;
       // if (this.editIndex === -1) {
       //   this.img = reader.result;
@@ -160,9 +170,8 @@ export class InputModalComponent implements OnInit {
 
   cleanUp() {
     this.editIndex = -1;
-    this.textEntry = '';
+    this.textEntry = "";
     this.img = null;
     this.showSearchResults = false;
   }
-
 }
